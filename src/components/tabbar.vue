21c7d8cf13e37815d2a023716333e5034bbd1705
bigrocs <bigrocs@qq.com>
2021-08-31 18:15:59 +0800

    自动提交 git 代码

---
+++ src/components/tabbar.vue
@@ -0,0 +1,35 @@
+<template>
+	<u-tabbar 
+		v-model="current" 
+		:list="list" 
+		:mid-button="true"
+		active-color="#409EFF"
+		inactive-color="#909399"
+		@change="change"
+	></u-tabbar>
+</template>
+
+<script>
+	export default {
+		props: {
+            list: {
+                type: Array,
+                default: 'home'
+            },
+		},
+		data() {
+			return {
+				current: 0,
+			}
+		},
+		methods: {
+			change(index) {
+				this.current = index
+                this.$emit('change',this.list[index])
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,21 +4,73 @@
 		<view>
 			<text class="title">{{title}}</text>
 		</view>
+		<tabbar
+			:list="list"
+			@change="navChange"
+		/>
 	</view>
 </template>
-
 <script>
+
+	import tabbar from '@/components/tabbar.vue'
 	export default {
+		components: { 
+			tabbar,
+		},
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				
+				list:[	// 商家导航
+					{
+						path: "sellerHome",	// 主页
+						iconPath: "home",
+						selectedIconPath: "home-fill",
+						text: '首页',
+						isDot: false,
+						customIcon: false,
+					},
+					{
+						path: "sellerOrder",	// 订单
+						iconPath: "file-text",
+						selectedIconPath: "file-text-fill",
+						text: '订单',
+						customIcon: false,
+					},
+					{
+						path: "sellerPay",	// 收银
+						iconPath: "rmb-circle",
+						selectedIconPath: "rmb-circle-fill",
+						text: '收银',
+						midButton: true,
+						customIcon: false,
+					},
+					{
+						path: "sellerReport",	// 报表
+						iconPath: "edit-pen",
+						selectedIconPath: "edit-pen-fill",
+						text: '报表',
+						customIcon: false,
+					},
+					{
+						path: "sellerMy",	// 我的
+						iconPath: "account",
+						selectedIconPath: "account-fill",
+						text: '我的',
+						count: 23,
+						isDot: false,
+						customIcon: false,
+					},
+				],
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			navChange(row){
+				console.log(row)
+			},
 		}
 	}
 </script>
